f3f2a34c63508fd38679e0dd2bdc8695b3badc1b
Lucas Swartsenburg <lswartsenburg@meta.com>
2023-11-20 16:24:05 -0500

    using one of the cipheres

--- api/app.py
+++ api/app.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from cipher_algorithms.ciphers.caesar.algo import caesar_cipher, Operation
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello again, World 2!</p>"
+    return caesar_cipher("HELLO WORLD", Operation.ENCRYPT, shift=1)
