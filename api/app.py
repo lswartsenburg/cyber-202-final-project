58fde1eed7d984b5c24f71e219bd222db9e160a9
Lucas Swartsenburg <504059+lswartsenburg@users.noreply.github.com>
2023-11-21 11:02:44 -0500

    Adding examples and better error handling (#2)
    Co-authored-by: Lucas Swartsenburg <lswartsenburg@meta.com>

--- api/app.py
+++ api/app.py
@@ -1,3 +1,5 @@
+import traceback
+
 from flask_openapi3 import Info
 from flask_openapi3 import OpenAPI, APIBlueprint
 from flask import redirect
@@ -6,12 +8,39 @@ from .ciphers.caesar import caesar_blueprint
 from .ciphers.vigenere import (
     vigenere_blueprint,
 )
+from .common.schemas import ExceptionSchema
+from cipher_algorithms.helpers.char_conversion_27 import InvalidCharacterException
 
 
 info = Info(title="Cipher Algorithm API", version="0.0.1")
 app = OpenAPI(__name__, info=info, doc_prefix="/docs", swagger_url="/")
 v1_api = APIBlueprint("api/v1", __name__, url_prefix="/api/v1")
 ciphers = APIBlueprint("ciphers", __name__, url_prefix="/ciphers")
+
+
+@app.errorhandler(InvalidCharacterException)
+def handle_invalid_char_exception(err):
+    error_response = ExceptionSchema(
+        code=422, name=f"{type(err)}", description=f"{err}"
+    )
+    return error_response.model_dump(), 422
+
+
+@app.errorhandler(Exception)
+def handle_exception(err):
+    app.logger.error(f"Unknown Exception: {str(err)}")
+    app.logger.debug(
+        "".join(
+            traceback.format_exception(etype=type(err), value=err, tb=err.__traceback__)
+        )
+    )
+    exception = ExceptionSchema(
+        code=500,
+        name="ServerError",
+        description="Aaaah, something went wrong. This one is on us.",
+    )
+
+    return exception.model_dump(), 500
 
 
 @app.route("/")
--- api/ciphers/caesar.py
+++ api/ciphers/caesar.py
@@ -1,11 +1,12 @@
 from flask_openapi3 import Tag
 
 from flask_openapi3 import APIBlueprint
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from cipher_algorithms.ciphers.caesar.algo import caesar_cipher, Operation, break_cipher
 from cipher_algorithms.helpers.dictionary import get_dictionary
-from cipher_algorithms.helpers.char_conversion_27 import InvalidCharacterException
+
+from ..common.schemas import ExceptionSchema
 
 caesar_blueprint = APIBlueprint("caesar", __name__, url_prefix="/caesar")
 
@@ -13,17 +14,17 @@ tag = Tag(name="Caesar cipher", description="Cipher functions for the Caesar cip
 
 
 class CaesarEncryptSchema(BaseModel):
-    message: str
-    shift: int
+    message: str = Field(json_schema_extra={"example": "MATT IS SHORTER THAN AARON"})
+    shift: int = Field(json_schema_extra={"example": 18})
 
 
 class CaesarDecryptSchema(BaseModel):
-    cipher: str
-    shift: int
+    cipher: str = Field(json_schema_extra={"example": "DSKKR JRJZFIKWIRKZSERSSIFE"})
+    shift: int = Field(json_schema_extra={"example": 18})
 
 
 class CaesarBreakSchema(BaseModel):
-    cipher: str
+    cipher: str = Field(json_schema_extra={"example": "DSKKR JRJZFIKWIRKZSERSSIFE"})
 
 
 class CaesarResultSchema(CaesarEncryptSchema, CaesarDecryptSchema):
@@ -34,64 +35,51 @@ class CaesarResultSchema(CaesarEncryptSchema, CaesarDecryptSchema):
     "/encrypt",
     tags=[tag],
     summary="Uses the Caesar cipher to encrypt a message",
-    responses={200: CaesarResultSchema},
+    responses={200: CaesarResultSchema, 422: ExceptionSchema},
 )
 def caesar_encrypt(body: CaesarEncryptSchema):
     """
     This is an encryption implementation of the Caesar cipther that accepts only upper case characters and space (27 characters). The
     space is at index 26.
     """
-    try:
-        cipher = caesar_cipher(
-            input_value=body.message, operation=Operation.ENCRYPT, shift=body.shift
-        )
-        response = CaesarResultSchema(
-            cipher=cipher, message=body.message, shift=body.shift
-        )
-        return response.model_dump(), 200
-    except Exception as e:
-        return f"Unexpected error {type(e)}:\n\n{e}", 422
+    cipher = caesar_cipher(
+        input_value=body.message, operation=Operation.ENCRYPT, shift=body.shift
+    )
+    response = CaesarResultSchema(cipher=cipher, message=body.message, shift=body.shift)
+    return response.model_dump(), 200
 
 
 @caesar_blueprint.post(
     "/decrypt",
     tags=[tag],
     summary="Uses the Caesar cipher to decrypt a message",
-    responses={200: CaesarResultSchema},
+    responses={200: CaesarResultSchema, 422: ExceptionSchema},
 )
 def caesar_decrypt(body: CaesarDecryptSchema):
     """
     This is an decryption implementation of the Caesar cipther that accepts only upper case characters and space (27 characters). The
     space is at index 26.
     """
-    try:
-        message = caesar_cipher(
-            input_value=body.cipher, operation=Operation.DECRYPT, shift=body.shift
-        )
-        response = CaesarResultSchema(
-            message=message, cipher=body.cipher, shift=body.shift
-        )
-        return response.model_dump(), 200
-    except Exception as e:
-        return f"Unexpected error {type(e)}:\n\n{e}", 422
+    message = caesar_cipher(
+        input_value=body.cipher, operation=Operation.DECRYPT, shift=body.shift
+    )
+    response = CaesarResultSchema(message=message, cipher=body.cipher, shift=body.shift)
+    return response.model_dump(), 200
 
 
 @caesar_blueprint.post(
     "/break",
     tags=[tag],
     summary="A somewhat efficient algorithm that breaks a caesar cipher",
-    responses={200: CaesarResultSchema},
+    responses={200: CaesarResultSchema, 422: ExceptionSchema},
 )
 def caesar_break(body: CaesarBreakSchema):
     """
     This implementation uses a word dictionary to break a Caesar cipher
     """
-    try:
-        words = get_dictionary()
-        result = break_cipher(body.cipher, words)
-        response = CaesarResultSchema(
-            message=result["message"], cipher=body.cipher, shift=result["shift"]
-        )
-        return response.model_dump(), 200
-    except Exception as e:
-        return f"Unexpected error {type(e)}:\n\n{e}", 422
+    words = get_dictionary()
+    result = break_cipher(body.cipher, words)
+    response = CaesarResultSchema(
+        message=result["message"], cipher=body.cipher, shift=result["shift"]
+    )
+    return response.model_dump(), 200
