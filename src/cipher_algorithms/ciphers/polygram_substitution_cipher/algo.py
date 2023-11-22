9b5b6af85636c3f1beecf2899d8f84f5a9234245
Chris Black <chris.black@lacework.net>
2023-11-22 13:51:00 -0800

    Updating code based per PR comments - will add more tests soon

--- src/cipher_algorithms/ciphers/polygram_substitution_cipher/algo.py
+++ src/cipher_algorithms/ciphers/polygram_substitution_cipher/algo.py
@@ -1,34 +1,43 @@
-# Chris Black - this is still WIP; will add more comments and tests.
+class UnsupportedCharacterError(Exception):
+    """Custom exception for unsupported characters."""
+    pass
 
 # Define the substitution scheme
-encryption_dict = {
+DEFAULT_ENCRYPTION_DICT = {
     'AB': 'XY',
     'CD': 'ZQ',
     # Add more substitutions here
 }
 
-# Create a decryption dictionary from the encryption dictionary
-decryption_dict = {v: k for k, v in encryption_dict.items()}
-
-def encrypt(text):
+def encrypt(text, key=None):
+    encryption_dict = key if key is not None else DEFAULT_ENCRYPTION_DICT
     encrypted = ""
     for i in range(0, len(text), 2):
         digram = text[i:i+2]
-        encrypted += encryption_dict.get(digram, digram)  # Default to original if not found
+        if digram not in encryption_dict and len(digram) == 2:
+            raise UnsupportedCharacterError(f"Unsupported characters found: {digram}")
+        encrypted += encryption_dict.get(digram, digram)
     return encrypted
 
-def decrypt(text):
+def decrypt(text, key=None):
+    encryption_dict = key if key is not None else DEFAULT_ENCRYPTION_DICT
+    decryption_dict = {v: k for k, v in encryption_dict.items()}
     decrypted = ""
     for i in range(0, len(text), 2):
         digram = text[i:i+2]
-        decrypted += decryption_dict.get(digram, digram)  # Default to original if not found
+        if digram not in decryption_dict and len(digram) == 2:
+            raise UnsupportedCharacterError(f"Unsupported characters found: {digram}")
+        decrypted += decryption_dict.get(digram, digram)
     return decrypted
 
 # Example usage
-plaintext = "ABCD"
-encrypted_text = encrypt(plaintext)
-decrypted_text = decrypt(encrypted_text)
+try:
+    plaintext = "ABCD"
+    encrypted_text = encrypt(plaintext)
+    decrypted_text = decrypt(encrypted_text)
 
-print(f"Plaintext: {plaintext}")
-print(f"Encrypted: {encrypted_text}")
-print(f"Decrypted: {decrypted_text}")
+    print(f"Plaintext: {plaintext}")
+    print(f"Encrypted: {encrypted_text}")
+    print(f"Decrypted: {decrypted_text}")
+except UnsupportedCharacterError as e:
+    print(f"Error: {e}")
