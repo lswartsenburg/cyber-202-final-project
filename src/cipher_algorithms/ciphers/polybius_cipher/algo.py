c420f708e1976657cb997c9cc1a5c97ee96bf6dc
shaosm12 <151479900+shaosm12@users.noreply.github.com>
2023-12-11 12:34:38 -0600

    Update algo.py

--- src/cipher_algorithms/ciphers/polybius_cipher/algo.py
+++ src/cipher_algorithms/ciphers/polybius_cipher/algo.py
@@ -44,3 +44,19 @@ def decrypt_polybius(cipher_text):
             plain_text += char
 
     return plain_text
+
+
+###Example###
+"""
+msg = "the quick brown fox jumps over the lazy dog"
+cipher = encrypt_polybius(msg)
+print(f'Plaintext: {plaintext}')
+print(f'Ciphertext: {cipher}')
+print (f'Decrypt: {decrypt_polybius(cipher)}' )
+
+---outputs the following---
+
+Plaintext: the quick brown fox jumps over the lazy dog
+Ciphertext: 44 23 15 41 45 24 13 25 12 42 34 52 33 21 34 53 24 45 32 35 43 34 51 15 42 44 23 15 31 11 55 54 14 34 22
+Decrypt: thequickbrownfoxiumpsoverthelazydog
+"""
