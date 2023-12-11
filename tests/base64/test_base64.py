51404766ee39851e452e9fb9cb8d8c6c4c4a7c57
Lucas Swartsenburg <lswartsenburg@meta.com>
2023-12-11 10:46:14 -0500

    Adding an additional assertion to base64

--- tests/base64/test_base64.py
+++ tests/base64/test_base64.py
@@ -11,8 +11,20 @@ def test_base64_encrypt():
     assert cipher == "dGVzdGluZ2FiYw=="
 
 
+def test_base64_encrypt_long():
+    plaintext_long = "this is an important message"
+    cipher_long = encrypt(plaintext_long)
+    assert cipher_long == "dGhpcyBpcyBhbiBpbXBvcnRhbnQgbWVzc2FnZQ=="
+
+
 def test_base64_decrypt():
     cipher = "dGVzdGluZ2FiYw=="
     plaintext = decrypt(cipher)
 
     assert plaintext == "testingabc"
+
+
+def test_base64_decrypt_long():
+    cipher_long = "dGhpcyBpcyBhbiBpbXBvcnRhbnQgbWVzc2FnZQ=="
+    plaintext_long = decrypt(cipher_long)
+    assert plaintext_long == "this is an important message"
